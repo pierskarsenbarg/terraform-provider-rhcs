9ee73254a25907d441a05cda69b2929753e4019e
Piers Karsenbarg <piers@pulumi.com>
2024-06-20 13:59:43 +0100

    Started updating ocm references in examples

--- generate_example_usages/resources/rhcs_identity_provider/resource.tf
+++ generate_example_usages/resources/rhcs_identity_provider/resource.tf
@@ -12,7 +12,7 @@ resource "rhcs_identity_provider" "htpasswd_idp" {
 }
 
 # github
-resource "ocm_identity_provider" "github_idp" {
+resource "rhcs_identity_provider" "github_idp" {
   cluster = "cluster-id-123"
   name    = "Github"
   github = {
@@ -23,7 +23,7 @@ resource "ocm_identity_provider" "github_idp" {
 }
 
 # gitlab
-resource "ocm_identity_provider" "gitlab_idp" {
+resource "rhcs_identity_provider" "gitlab_idp" {
   cluster = "cluster-id-123"
   name    = "GitLab"
   gitlab = {
@@ -34,7 +34,7 @@ resource "ocm_identity_provider" "gitlab_idp" {
 }
 
 # google
-resource "ocm_identity_provider" "google_idp" {
+resource "rhcs_identity_provider" "google_idp" {
   cluster = "cluster-id-123"
   name = "google"
   google = {
